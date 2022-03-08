e61e6e60b140465aad3d93fd99dc3aa80995b348
SalisYB <salisbell404@gmail.com>
2022-03-08 13:53:40 +0100

    make it standalone app

--- hcb/settings.py
+++ hcb/settings.py
@@ -37,9 +37,11 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'corsheaders'
     'rest_framework',
     'knox',
     'users',
+    'frontend'
 ]
 
 MIDDLEWARE = [
@@ -124,3 +126,8 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+CORS_ORIGIN_WHITELIST = [
+    'http://localhost:3000'
+]
--- hcb/urls.py
+++ hcb/urls.py
@@ -1,7 +1,9 @@
+from django import urls
 from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/', include('users.urls'))
+    path('api/', include('users.urls')),
+    path('', include('frontend.urls'))
 ]
